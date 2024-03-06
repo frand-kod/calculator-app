4687356ba06b6cea68e86c6c091069a881dac8b1
frand-kod <frandkampus99@gmail.com>
2024-03-06 17:01:30 +0700

    Menambahkan int

--- kakulator.cs
+++ kakulator.cs
@@ -2,6 +2,7 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
+		int a=2,b=2;
 		Console.WriteLine("hasil penambahan " + a + "+" + b + " = " + Penambahan(a, b));
 		Console.WriteLine("hasil pengurangan " + a + "-" + b + " = " + Pengurangan(a, b));
 	}
